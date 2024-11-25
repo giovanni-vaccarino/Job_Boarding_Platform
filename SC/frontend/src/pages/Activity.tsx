fe096cb0d2b2a3b65c0116919cb2655d0e489a19
nicolovacis <vacis.nicolo@icloud.com>
2024-11-25 12:25:36 +0100

    Matches, Home, Activity pages made reliable when NO DATA to display

--- SC/frontend/src/pages/Activity.tsx
+++ SC/frontend/src/pages/Activity.tsx
@@ -1,80 +1,29 @@
 import { Page } from '../components/layout/Page.tsx';
 import { TitleHeader } from '../components/page-headers/TitleHeader.tsx';
 import { JobsTable } from '../components/tables/JobsTable.tsx';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+
+const exampleData = [];
 
 export const Activity = () => {
-  const exampleData = [
-    {
-      title: 'Software Engineer',
-      company: 'TechCorp',
-      state: 'Open',
-      location: 'New York, NY',
-      submissionDate: '2024-11-20',
-    },
-    {
-      title: 'Data Scientist',
-      company: 'DataWorks',
-      state: 'Closed',
-      location: 'San Francisco, CA',
-      submissionDate: '2024-11-15',
-    },
-    {
-      title: 'Product Manager',
-      company: 'InnovateNow',
-      state: 'Pending',
-      location: 'Austin, TX',
-      submissionDate: '2024-11-10',
-    },
-    {
-      title: 'Software Engineer',
-      company: 'TechCorp',
-      state: 'Open',
-      location: 'New York, NY',
-      submissionDate: '2024-11-20',
-    },
-    {
-      title: 'Data Scientist',
-      company: 'DataWorks',
-      state: 'Closed',
-      location: 'San Francisco, CA',
-      submissionDate: '2024-11-15',
-    },
-    {
-      title: 'Product Manager',
-      company: 'InnovateNow',
-      state: 'Pending',
-      location: 'Austin, TX',
-      submissionDate: '2024-11-10',
-    },
-    {
-      title: 'Software Engineer',
-      company: 'TechCorp',
-      state: 'Open',
-      location: 'New York, NY',
-      submissionDate: '2024-11-20',
-    },
-    {
-      title: 'Data Scientist',
-      company: 'DataWorks',
-      state: 'Closed',
-      location: 'San Francisco, CA',
-      submissionDate: '2024-11-15',
-    },
-    {
-      title: 'Product Manager',
-      company: 'InnovateNow',
-      state: 'Pending',
-      location: 'Austin, TX',
-      submissionDate: '2024-11-10',
-    },
-  ];
-
   return (
     <Page>
       <TitleHeader title={'My Jobs List'} />
-      <Box sx={{ width: '90%', margin: '0 auto', marginTop: '1rem' }}>
-        <JobsTable jobs={exampleData} />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '90%',
+          margin: '0 auto',
+          marginTop: '1rem',
+        }}
+      >
+        {exampleData.length > 0 ? (
+          <JobsTable jobs={exampleData} />
+        ) : (
+          <Typography sx={{ fontStyle: 'italic' }}>NO DATA</Typography>
+        )}
       </Box>
     </Page>
   );
--- SC/frontend/src/pages/Matches.tsx
+++ SC/frontend/src/pages/Matches.tsx
@@ -1,41 +1,88 @@
-﻿import { Box, Stack } from '@mui/material';
+﻿import { Box, Stack, Typography } from '@mui/material';
 import { Page } from '../components/layout/Page.tsx';
 import { JobListItem } from '../components/list-items/JobListItem.tsx';
 import { TitleHeader } from '../components/page-headers/TitleHeader.tsx';
 import { ImportantJobListItem } from '../components/list-items/ImportantJobListItem.tsx';
 
+const importantJobList = [
+  {
+    companyName: 'Amazon',
+    jobTitle: 'Software Engineer',
+    location: 'Chicago',
+    datePosted: '2 weeks ago',
+  },
+];
+
+const jobList = [
+  {
+    companyName: 'Amazon',
+    jobTitle: 'Software Engineer',
+    location: 'Chicago',
+    datePosted: '2 weeks ago',
+  },
+  {
+    companyName: 'Amazon',
+    jobTitle: 'Software Engineer',
+    location: 'Chicago',
+    datePosted: '2 weeks ago',
+  },
+  {
+    companyName: 'Amazon',
+    jobTitle: 'Software Engineer',
+    location: 'Chicago',
+    datePosted: '2 weeks ago',
+  },
+  {
+    companyName: 'Amazon',
+    jobTitle: 'Software Engineer',
+    location: 'Chicago',
+    datePosted: '2 weeks ago',
+  },
+  {
+    companyName: 'Amazon',
+    jobTitle: 'Software Engineer',
+    location: 'Chicago',
+    datePosted: '2 weeks ago',
+  },
+];
+
 export const Matches = () => {
   return (
     <Page>
-      <Box
-        sx={{
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          mb: '3rem',
-        }}
-      >
-        <TitleHeader title={'Invites'} />
-
-        <Stack
-          direction="column"
-          spacing={2}
+      {Object.keys(importantJobList).length > 0 && (
+        <Box
           sx={{
             width: '100%',
-            mt: '3rem',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
             alignItems: 'center',
+            mb: '3rem',
           }}
         >
-          <ImportantJobListItem
-            companyName={'Amazon'}
-            jobTitle={'Software Engineer'}
-            location={'Chicago'}
-            datePosted={'2 weeks ago'}
-          />
-        </Stack>
-      </Box>
+          <TitleHeader title={'Invites'} />
+
+          <Stack
+            direction="column"
+            spacing={2}
+            sx={{
+              width: '100%',
+              mt: '3rem',
+              alignItems: 'center',
+            }}
+          >
+            {importantJobList.map((job, index) => (
+              <ImportantJobListItem
+                key={index}
+                companyName={job.companyName}
+                jobTitle={job.jobTitle}
+                location={job.location}
+                datePosted={job.datePosted}
+              />
+            ))}
+          </Stack>
+        </Box>
+      )}
 
       <Box
         sx={{
@@ -47,7 +94,7 @@ export const Matches = () => {
           mb: '1rem',
         }}
       >
-        <TitleHeader title={'Jobs for you'} />
+        <TitleHeader title="Jobs for you" />
 
         <Stack
           direction="column"
@@ -58,40 +105,19 @@ export const Matches = () => {
             alignItems: 'center',
           }}
         >
-          <JobListItem
-            companyName={'Amazon'}
-            jobTitle={'Software Engineer'}
-            location={'Chicago'}
-            datePosted={'2 weeks ago'}
-          />
-
-          <JobListItem
-            companyName={'Amazon'}
-            jobTitle={'Software Engineer'}
-            location={'Chicago'}
-            datePosted={'2 weeks ago'}
-          />
-
-          <JobListItem
-            companyName={'Amazon'}
-            jobTitle={'Software Engineer'}
-            location={'Chicago'}
-            datePosted={'2 weeks ago'}
-          />
-
-          <JobListItem
-            companyName={'Amazon'}
-            jobTitle={'Software Engineer'}
-            location={'Chicago'}
-            datePosted={'2 weeks ago'}
-          />
-
-          <JobListItem
-            companyName={'Amazon'}
-            jobTitle={'Software Engineer'}
-            location={'Chicago'}
-            datePosted={'2 weeks ago'}
-          />
+          {jobList.length > 0 ? (
+            jobList.map((job, index) => (
+              <JobListItem
+                key={index}
+                companyName={job.companyName}
+                jobTitle={job.jobTitle}
+                location={job.location}
+                datePosted={job.datePosted}
+              />
+            ))
+          ) : (
+            <Typography sx={{ fontStyle: 'italic' }}>NO DATA</Typography>
+          )}
         </Stack>
       </Box>
     </Page>
