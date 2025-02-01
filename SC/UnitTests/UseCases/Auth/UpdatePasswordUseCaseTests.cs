524e0fee22086ab4ae9b68d4f83b4b67ca917119
Gianni <giovannivaccarino03@gmail.com>
2025-02-01 21:21:23 +0100

    Final Unit Tests

---
+++ SC/UnitTests/UseCases/Auth/UpdatePasswordUseCaseTests.cs
@@ -0,0 +1,64 @@
+﻿using backend.Business.Auth.UpdatePasswordUseCase;
+using backend.Business.Auth.VerifyMailUseCase;
+using backend.Data;
+using backend.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTests.UseCases.Auth;
+
+public class UpdatePasswordUseCaseTests
+{
+    private readonly IsolatedUseCaseTestServices<UpdatePasswordUseCase> _services;
+    private readonly AppDbContext _dbContext;
+    private readonly UpdatePasswordUseCase _updatePasswordUseCase;
+
+    /// <summary>
+    /// Initializes a new instance of the class, setting up the testing environment.
+    /// </summary>
+    public UpdatePasswordUseCaseTests()
+    {
+        _services = new IsolatedUseCaseTestServices<UpdatePasswordUseCase>("UpdatePasswordUseCaseTests");
+        _dbContext = _services.DbContext;
+        _updatePasswordUseCase = (UpdatePasswordUseCase)Activator.CreateInstance(
+            typeof(UpdatePasswordUseCase), _dbContext, _services.SecurityContext, _services.LoggerMock.Object)!;
+    }
+
+    [Fact(DisplayName = "Successfully update password")]
+    public async Task Should_Update_Password()
+    {
+        var user = new User
+        {
+            Email = "student@gmail.com",
+            PasswordHash = "Password123!",
+            Verified = false
+        };
+       
+        _dbContext.Users.Add(user);
+        await _dbContext.SaveChangesAsync();
+         
+        var verificationToken = _services.SecurityContext.CreateVerificationToken(user.Id.ToString());
+         
+        var newPassword = "NewPassword";
+        var command = new UpdatePasswordCommand(verificationToken, newPassword);
+         
+        await _updatePasswordUseCase.Handle(command, CancellationToken.None);
+         
+        var verifiedUser = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == user.Id);
+        Assert.NotNull(verifiedUser);
+        Assert.NotEqual("Password123!", verifiedUser.PasswordHash);
+    }
+    
+    [Fact(DisplayName = "Successfully verify mail")]
+    public async Task Should_Throw_Exception_If_User_Not_Found()
+    {
+        var verificationToken = _services.SecurityContext.CreateVerificationToken("1");
+         
+        var newPassword = "NewPassword";
+        var command = new UpdatePasswordCommand(verificationToken, newPassword);
+         
+        var act = async() => await _updatePasswordUseCase.Handle(command, CancellationToken.None);
+
+        
+        await Assert.ThrowsAsync<KeyNotFoundException>(act);
+    }
+}
---
+++ SC/UnitTests/UseCases/Auth/VerifyMailUseCaseTests.cs
@@ -0,0 +1,61 @@
+﻿using backend.Business.Auth.VerifyMailUseCase;
+using backend.Data;
+using backend.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTests.UseCases.Auth;
+
+public class VerifyMailUseCaseTests
+{
+    private readonly IsolatedUseCaseTestServices<VerifyMailUseCase> _services;
+    private readonly AppDbContext _dbContext;
+    private readonly VerifyMailUseCase _verifyMailUseCase;
+
+    /// <summary>
+    /// Initializes a new instance of the class, setting up the testing environment.
+    /// </summary>
+    public VerifyMailUseCaseTests()
+    {
+        _services = new IsolatedUseCaseTestServices<VerifyMailUseCase>("VerifyMailUseCaseTests");
+        _dbContext = _services.DbContext;
+        _verifyMailUseCase = (VerifyMailUseCase)Activator.CreateInstance(
+            typeof(VerifyMailUseCase), _dbContext, _services.SecurityContext, _services.LoggerMock.Object)!;
+    }
+
+    [Fact(DisplayName = "Successfully verify mail")]
+    public async Task Should_Verify_Mail()
+    {
+        
+        var user = new User
+        {
+            Email = "student@gmail.com",
+            PasswordHash = "Password123!",
+            Verified = false
+        };
+       
+         _dbContext.Users.Add(user);
+         await _dbContext.SaveChangesAsync();
+         
+         var verificationToken = _services.SecurityContext.CreateVerificationToken(user.Id.ToString());
+         
+         var command = new VerifyMailCommand(verificationToken);
+         
+         await _verifyMailUseCase.Handle(command, CancellationToken.None);
+         
+         var verifiedUser = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == user.Id);
+         Assert.NotNull(verifiedUser);
+         Assert.True(verifiedUser.Verified);
+    }
+    
+    [Fact(DisplayName = "Successfully verify mail")]
+    public async Task Should_Throw_Exception_If_User_Not_Found()
+    {
+        var verificationToken = _services.SecurityContext.CreateVerificationToken("1");
+         
+        var command = new VerifyMailCommand(verificationToken);
+         
+        var act = async () => await _verifyMailUseCase.Handle(command, CancellationToken.None);
+        
+        await Assert.ThrowsAsync<KeyNotFoundException>(act);
+    }
+}
